653c1f487d452c1edb2fba4811bc70dac2b9d74b
Lahiru <lahirupeiris88@gmail.com>
2025-05-05 02:17:48 +0100

    REST API Best Practices. Automapper and Repositories

--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -1,4 +1,7 @@
+using HotelListing.API.Configurations;
+using HotelListing.API.Contracts;
 using HotelListing.API.Data;
+using HotelListing.API.Repository;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -32,7 +35,9 @@ builder.Host.UseSerilog((context, services, configuration) =>
         .Enrich.FromLogContext()
         .WriteTo.Console();
 });
-
+builder.Services.AddAutoMapper(typeof(MapperConfig));
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
